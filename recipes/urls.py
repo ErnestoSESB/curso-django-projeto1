322b71d87c92765d65ec32a41b435e6960799841
Silvio <silvioebis2005@gmail.com>
2025-05-17 23:52:54 -0300

    testes e limpeza de ambiente

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,12 +1,9 @@
 from django.contrib import admin
 from django.urls import path
-from django.http import HttpResponse
-from recipes.views import home, nada, my_view
+from recipes.views import home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('sobre/', my_view),
     path('', home),
-    path('home/', nada)
 ]
 
