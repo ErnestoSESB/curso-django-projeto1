4815db37ed4e06521c174496e815ab909ba997e4
Silvio <silvioebis2005@gmail.com>
2025-05-21 22:10:49 -0300

    terminando os ajustes dos caminhos

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.urls import path
-from recipes.views import home
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home),
+    path('', views.home),
+    path('recipes/',views.recipes)
 ]
 
--- recipes/views.py
+++ recipes/views.py
@@ -4,3 +4,8 @@ def home(request):
     return render(request, 'pages/home.html', context={
     'name': 'Silvio Ernesto'
 })
+
+def recipes(request):
+    return render(request, 'pages/home.html', context={
+    'name': 'Silvio Ernesto'
+})
