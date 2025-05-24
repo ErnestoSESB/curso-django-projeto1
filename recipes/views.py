2ecf30c2909f54f84062b32b26b5e998bd3b7dd0
Silvio <silvioebis2005@gmail.com>
2025-05-24 02:24:52 -0300

    começo da inserção de dados ficticíos

--- recipes/views.py
+++ recipes/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
+from utils.recipes.factory import make_recipe
 
 def home(request):
     return render(request, 'pages/home.html', context={
-    'name': 'Silvio Ernesto'
+    'recipes': [make_recipe() for i in range(10)],
 })
 def recipes(request, id):
     return render(request, 'pages/recipe-view.html', context={
-    'name': 'Silvio Ernesto'
+    'recipe': make_recipe(),
 })
